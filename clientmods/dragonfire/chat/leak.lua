35c586625fe8b941b74695b78a941d2c4e77dea5
cora <email@example.com>
2020-10-25 11:24:56 +0100

    removed dupe csms, changed "Leak" to be a chatcommand

--- clientmods/dragonfire/chat/leak.lua
+++ clientmods/dragonfire/chat/leak.lua
@@ -1,12 +1,26 @@
 local etime = 0
 
-minetest.register_globalstep(function(dtime)
+--[[minetest.register_globalstep(function(dtime)
 	if not minetest.settings:get_bool("leak") then return end
 	etime = etime + dtime
 	if etime < 5 then return end
 	etime = 0
 	local player = minetest.localplayer
 	minetest.send_chat_message(minetest.pos_to_string(vector.floor(player:get_pos())))
-end)  
+end)--]]
+minetest.register_chatcommand("cleak", {
+        params = "<pos>",
+            description = "Open node inventory metadata at position.",
+                func = cleak
+            })
+local function cleak()
+	if not minetest.settings:get_bool("leak") then return end
+	etime = etime + dtime
+	if etime < 5 then return end
+	etime = 0
+	local player = minetest.localplayer
+	minetest.send_chat_message(minetest.pos_to_string(vector.floor(player:get_pos())))
+end  
 
-minetest.register_cheat("Leak", "Player", "leak")
+
+--minetest.register_cheat("Leak", "Player", "leak")
