d6f3724f47c10f943ab3c76dfa50c79e344ec858
cora <email@example.com>
2020-10-25 11:28:47 +0100

    cleak: fix

--- clientmods/dragonfire/chat/leak.lua
+++ clientmods/dragonfire/chat/leak.lua
@@ -8,19 +8,14 @@ local etime = 0
 	local player = minetest.localplayer
 	minetest.send_chat_message(minetest.pos_to_string(vector.floor(player:get_pos())))
 end)--]]
+function cleak()
+	minetest.send_chat_message(minetest.pos_to_string(vector.floor(player:get_pos())))
+end  
 minetest.register_chatcommand("cleak", {
         params = "<pos>",
             description = "Open node inventory metadata at position.",
                 func = cleak
             })
-local function cleak()
-	if not minetest.settings:get_bool("leak") then return end
-	etime = etime + dtime
-	if etime < 5 then return end
-	etime = 0
-	local player = minetest.localplayer
-	minetest.send_chat_message(minetest.pos_to_string(vector.floor(player:get_pos())))
-end  
 
 
 --minetest.register_cheat("Leak", "Player", "leak")
