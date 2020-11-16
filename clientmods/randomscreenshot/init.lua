edc010121c8951b35a7b64d8bcf515be3ef15989
cora <email@example.com>
2020-11-16 12:40:45 +0100

    randomscreenshot: make it nice :)

--- clientmods/randomscreenshot/init.lua
+++ clientmods/randomscreenshot/init.lua
@@ -4,16 +4,30 @@
 
 randomscreenshot = {}
 
-local interval=10 -- minimum number of minutes to wait til next screenshot
-local rnd=10 --random time
-local nextsc=0
+local function init_settings(setting_table)
+    for k, v in pairs(setting_table) do
+        if minetest.settings:get(k) == nil then
+            if type(v) == "boolean" then
+                minetest.settings:set_bool(k, v)
+            else
+                minetest.settings:set(k, v)
+            end
+        end
+    end
+end
 
+init_settings({
+    randomscreenshot_interval = 10,
+    randomscreenshot_rnd = 10
+})
+
+local nextsc=0
 
 minetest.register_globalstep(function()
     if not minetest.settings:get_bool("randomsc") then return end
     if os.time() < nextsc then return end
     math.randomseed(os.clock())
-    nextsc=os.time() + ( interval * 60 ) + math.random(rnd * 60)
+    nextsc=os.time() + ( minetest.settings:get('randomscreenshot_interval') * 60 ) + math.random(minetest.settings:get('randomscreenshot_rnd') * 60)
     minetest.after("15.0",function()
         minetest.hide_huds()
         minetest.display_chat_message("\n\n\n\n\n\n\n\n\n")
