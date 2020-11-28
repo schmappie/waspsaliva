8155cf4ebc7542ce09b50e33fdff80211b52650e
cora <email@example.com>
2020-11-28 01:15:46 +0100

    scaffold: add railscaffold

--- clientmods/scaffold/railscaffold.lua
+++ clientmods/scaffold/railscaffold.lua
@@ -8,10 +8,23 @@ local saplings = {
     "mcl_minecarts:golden_rail"
 }
 
+local function checknode(pos)
+    local node = minetest.get_node_or_nil(pos)
+    if node and node.name ~= "air" and node.name ~="mesecons_torch:redstoneblock" and node.name ~= "mcl_minecarts:golden_rail"  then return true end
+    return false
+end
+
 scaffold.register_template_scaffold("RailScaffold", "scaffold_rails", function(below)
     local lp = vector.round(minetest.localplayer:get_pos())
-
     if scaffold.place_if_needed(dirt, below) then
         scaffold.place_if_needed(saplings, lp)
     end
+    local fpos1=turtle.dircoord(1,1,0)
+    local fpos2=turtle.dircoord(1,-1,0)
+    local fpos3=turtle.dircoord(1,0,0)
+    if checknode(fpos1) then minetest.after(0.4,function()
+     minetest.localplayer:set_wield_index(0)
+     minetest.dig_node(fpos1) end) end
+    if checknode(fpos2) then minetest.after(0.45,function() minetest.dig_node(fpos2) end) end
+    if checknode(fpos3) then minetest.after(0.5,function() minetest.dig_node(fpos3) end) end
 end)
