1cbc11e410d6eeb1679cda7d3d388745cb205d45
cora <email@example.com>
2020-11-30 00:59:10 +0100

    crystalkamikaze: add tnt removal, cleanup, optimisation

--- clientmods/crystalkamikaze/init.lua
+++ clientmods/crystalkamikaze/init.lua
@@ -1,8 +1,10 @@
 local fnd=false
 local cpos={x=0,y=0,z=0}
-local crange=250
+local crange=500
 local hud_wp=nil
 local zz={x=0,y=64,z=0}
+local badnodes={'mcl_tnt:tnt'}
+local searchheight=250
 
 local function set_kwp(name,pos)
     if hud_wp then
@@ -18,11 +20,20 @@ local function set_kwp(name,pos)
         })
     end
 end
+local nextzz=0
+local function randomzz()
+    if nextzz > os.clock() then return false end
+    math.randomseed(os.time())
+    zz.y=math.random(64,searchheight)
+    nextzz=os.clock()+ 15
+end
 
-local function find_crystal()
-    if fnd then return cpos end
+local function find_bad_things()
+    if fnd then return true end
     local obs=minetest.localplayer.get_nearby_objects(crange)
-    for k, v in ipairs(obs) do
+    local lp=minetest.localplayer:get_pos()
+
+    for k, v in ipairs(obs) do -- look for crystals first
 		if ( v:get_item_textures():find("mcl_end_crystal") ) then
                 cpos=v:get_pos()
                 set_kwp(v:get_item_textures(),v:get_pos())
@@ -30,6 +41,7 @@ local function find_crystal()
                 return true
         end
     end
+
     for k, v in ipairs(obs) do
 		if ( v:get_item_textures():find("arrow_box") ) then
                 cpos=v:get_pos()
@@ -39,47 +51,75 @@ local function find_crystal()
         end
     end
 
-    --minetest.display_chat_message("crystalKamikaze: nothing found. flying to 0,0,0")
+    local epos=minetest.find_nodes_in_area(vector.add(lp,{x=79,y=79,z=79}), vector.add(lp,{x=-79,y=-79,z=-79}), badnodes, true)
+    if epos then
+        for k,v in pairs(epos) do for kk,vv in pairs(v) do
+            cpos=vv
+            set_kwp('mcl_tnt:tnt',cpos)
+            fnd=true
+            return true
+        end end
+    end
+
+    set_kwp('nothing found',zz)
+    randomzz()
     fnd=false
     return false
 end
 
+
+
+local function flythere()
+    minetest.settings:set_bool('noclip',true)
+    minetest.settings:set_bool("pitch_move",true)
+    minetest.settings:set_bool("continuous_forward",true)
+    autofly.aim(cpos)
+    core.set_keypress("special1", true)
+end
+
+local function stopflight()
+            minetest.settings:set_bool("continuous_forward",false)
+            minetest.settings:set_bool("noclip",false)
+            minetest.settings:set_bool("pitch_move",false)
+            core.set_keypress("special1", false)
+            minetest.after("0",function()
+                minetest.interact("start_digging")
+                minetest.dig_node(cpos)
+                fnd=false
+            end)
+end
+
 minetest.register_globalstep(function()
-    if not minetest.settings:get_bool("crystalkamikaze") and not minetest.localplayer:get_name():find("kamikaze") then return end
+    if not minetest.settings:get_bool("crystalkamikaze") and not minetest.localplayer:get_name():find("kamikaze") then
+        if hud_wp then
+            minetest.localplayer:hud_remove(hud_wp) hud_wp=nil
+            minetest.settings:set_bool("continuous_forward",false)
+            minetest.settings:set_bool("noclip",false)
+            minetest.settings:set_bool("pitch_move",false)
+            core.set_keypress("special1", false)
+            fnd=false
+        end
+        return
+    end
 
     local lp = minetest.localplayer:get_pos()
-    if not find_crystal() then
-        set_kwp('nothing found',zz)
+    if not find_bad_things() then
         if vector.distance(lp,zz) < 1 then
-            minetest.settings:set_bool("continuous_forward",false)
-            core.set_keypress("special1", false)
+            stopflight()
+            return
         else
-            autofly.aim(zz)
-            minetest.settings:set_bool('noclip',true)
-            minetest.settings:set_bool("pitch_move",true)
-            minetest.settings:set_bool("continuous_forward",true)
+            cpos=zz
         end
     elseif vector.distance(lp,cpos) < 1 then
-        minetest.settings:set_bool("continuous_forward",false)
-        core.set_keypress("special1", false)
-        fnd=false
-        minetest.after("2.0",function() if fnd then find_crystal() end end)
-    else
-        minetest.settings:set_bool('noclip',true)
-        minetest.settings:set_bool("pitch_move",true)
-        minetest.settings:set_bool("continuous_forward",true)
-        autofly.aim(cpos)
-        core.set_keypress("special1", true)
+        stopflight()
+        return
     end
+    flythere()
 end)
 
 minetest.register_on_death(function()
     if not minetest.settings:get_bool("crystalkamikaze") then return end
     fnd=false
 end)
-
-
-
-minetest.register_chatcommand('sctest',{func=function() minetest.localplayer:set_control() end})
 
 minetest.register_cheat("CrystalKamikaze", "Combat", "crystalkamikaze")
